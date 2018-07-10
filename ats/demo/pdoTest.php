4b45a24e448c0db18abb7911f7c75e0c45e05141
ss7424Refar <812647742@qq.com>
2018-07-10 17:29:34 +0800

    coding in sad day

---
+++ ats/demo/pdoTest.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JuL
+ * Date: 7/10/2018
+ * Time: 4:32 PM
+ */
+
--- ats/function/dbConnect.php
+++ ats/function/dbConnect.php
@@ -2,19 +2,13 @@
 require_once '../config_db.inc.php';
 
 function getDbConnect(){
-    $host = constant('DB_HOST');
-    $user = constant('DB_USER');
-    $pwd = constant('DB_PASS');
-    $charset = constant('DB_CHARSET');
-    $dbName = constant('DB_NAME');
-    $port = constant('DB_PORT');
 
-    $conn = mysqli_connect($host, $user, $pwd, $dbName, $port);
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
     if (!$conn) {
         die('could not connect'. mysqli_connect_error());
     }
-    mysqli_set_charset($conn, $charset);
+    mysqli_set_charset($conn, DB_CHARSET);
 
     return $conn;
 }
@@ -25,7 +19,7 @@ function getPDOConnect(){
     $pwd = constant('DB_PASS');
     $dbName = constant('DB_NAME');
 
-    $dbh = 'mysql:host='. $host .';dbname='. $dbName;
+    $dbh = 'mysql:host='. DB_HOST .';dbname='. DB_NAME;
     try {
         $pdoc = new PDO($dbh, $user, $pwd);
         return $pdoc;
